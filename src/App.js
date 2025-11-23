12e68241a9cd5d7a396c604cc1dae5bf0f078f1d
psasaslsask <127711403+psasaslsask@users.noreply.github.com>
2025-11-22 22:57:31 -0500

    Restrict restaurant dashboard to restaurant users

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
           <Route
             path="/restaurant-dashboard"
             element={
-              <ProtectedRoute>
+              <ProtectedRoute restaurantOnly>
                 <RestaurantDashboard />
               </ProtectedRoute>
             }
